16dd84be31cca772c76909a1dc0aa861297208e0
TOKUJI <14055040+TOKUJI@users.noreply.github.com>
2018-10-11 16:47:15 +0000

    add user specific configuration script.

---
+++ data/after.provisioning.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# VirtualBox guest additions.
+VBoxClient-all
+
+# Font install
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri --noconfirm
+pikaur --noconfirm -S ttf-myrica --noconfirm
+
+# Git configuration
+git config --global user.email "14055040+TOKUJI@users.noreply.github.com"
+git config --global user.name "TOKUJI"
