5b28793b3807fbf431be08a240e9569cc02ce8be
TOKUJI <14055040+TOKUJI@users.noreply.github.com>
2018-10-12 17:05:39 +0000

    settings for virtual box guest additions and fcitx are added

--- data/after.provisioning.sh
+++ data/after.provisioning.sh
@@ -1,7 +1,4 @@
-#! /bin/sh
-
-# VirtualBox guest additions.
-VBoxClient-all
+#! /bin/sh +x
 
 # Font install
 git clone https://aur.archlinux.org/pikaur.git
@@ -12,3 +9,6 @@ pikaur --noconfirm -S ttf-myrica --noconfirm
 # Git configuration
 git config --global user.email "14055040+TOKUJI@users.noreply.github.com"
 git config --global user.name "TOKUJI"
+
+# IME support
+
